cadf561f2622c2866a3bc9b83f8b682ace613775
TEDDY <teddy.mbugua@student.moringaschool.com>
2023-05-09 18:21:39 +0300

    done

--- db/migrate/20210506153127_create_rides.rb
+++ db/migrate/20210506153127_create_rides.rb
@@ -1,6 +1,9 @@
-class CreateRides < ActiveRecord::Migration[6.1]
+# db/migrate/create_rides.rb
+class CreateRides < ActiveRecord::Migration[6.0]
   def change
     create_table :rides do |t|
+      t.belongs_to :taxi, foreign_key: true
+      t.belongs_to :passenger, foreign_key: true
 
       t.timestamps
     end
